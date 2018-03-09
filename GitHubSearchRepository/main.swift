a0b6e3c1d17cf1e87b42edcab44fbdd4094c66d6
izibazi <izibazi@gmail.com>
2018-03-09 19:36:25 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by Yasunari Ishibashi on 2018/03/09.
+//  Copyright © 2018年 Yasunari Ishibashi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
